ff95bc298ac47bad4bde3f8f697630b37bb51ae0
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-07-21 08:12:55 +0100

    Added event model

---
+++ src/models/event.ts
@@ -0,0 +1,20 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface EventModel extends Document {
+  _id: string;
+  name: string;
+  date: Date;
+  location: string;
+  directions?: string;
+  details?: string;
+}
+
+const eventSchema = new Schema<EventModel>({
+  name: { type: String, required: true },
+  date: { type: Date, required: true },
+  location: { type: String, required: true },
+  directions: { type: String },
+  details: { type: String },
+});
+
+export const Event = model<EventModel>('Event', eventSchema);
