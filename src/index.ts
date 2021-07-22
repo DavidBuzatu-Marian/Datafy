dbc54c8d54927b87d1bbf950bcb73ecfabfd1828
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-07-22 08:30:57 +0100

    Added logger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from './server/server';
 import * as dotenv from 'dotenv';
 import { connectToDatabase } from './database/connect';
+import { logger } from './logger/logger';
 
 connectToDatabase();
 dotenv.config();
@@ -8,5 +9,5 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
-  console.log(`Server started on port:${port}`);
+  logger.info(`Server started on port:${port}`);
 });
