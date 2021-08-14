b6052c04cb342e553eb88dc48bab9e95dadfdf3f
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-08-14 22:13:31 +0100

    Added publish endpoint

--- src/routes/api/blog.ts
+++ src/routes/api/blog.ts
@@ -4,20 +4,17 @@ import { Request, Response } from 'express';
 import { check } from 'express-validator';
 import {
   changeDirectory,
-  gitCommitLatestChangesAndSwitchToMain,
-  pullLatestChangesToBlog,
-  replaceAll,
-  switchToBlogTitleBranch,
-  updateAndRebaseBlogWithRemote,
-  writeBlogToFile,
+  gitMergeBranchIntoMain,
+  gitPushBranch,
+  saveBlog,
 } from '../handlers/blog';
 
 const router = express.Router();
 
-class Blog {
-  title: String;
+export class Blog {
+  title: string;
   content: String;
-  getTitle(): String {
+  getTitle(): string {
     return this.title;
   }
   getContent(): String {
@@ -36,18 +33,34 @@ router.put(
       if (checkNotSucceeded(req)) {
         return res.status(400).json('Error! Checks have failed!');
       }
-      const blog: Blog = Object.assign(new Blog(), req.body);
-      const blogTitleWithDash = blog.title.split(' ').join('-');
+      let blog: Blog = Object.assign(new Blog(), req.body);
+      blog.title = blog.title.split(' ').join('-');
+      saveBlog(blog);
 
-      pullLatestChangesToBlog(res);
-      updateAndRebaseBlogWithRemote(res);
+      return res.status(200).json(`Blog: ${blog.title} saved successfully!`);
+    } catch (err) {
+      handleErrors(res, err);
+    }
+  }
+);
+
+router.put(
+  '/publish',
+  [
+    check('title', 'Title is mandatory').not().isEmpty(),
+    check('content', 'Content is mandatory').not().isEmpty(),
+  ],
+  async (req: Request, res: Response) => {
+    try {
+      if (checkNotSucceeded(req)) {
+        return res.status(400).json('Error! Checks have failed!');
+      }
+      let blog: Blog = Object.assign(new Blog(), req.body);
+      blog.title = blog.title.split(' ').join('-');
+      saveBlog(blog);
       changeDirectory('Blogs');
-      switchToBlogTitleBranch(res, blogTitleWithDash);
-      writeBlogToFile(res, blog.content, blogTitleWithDash);
-      replaceAll(res, /\\n/g, '\n\n', blogTitleWithDash);
-      replaceAll(res, /\"/g, '', blogTitleWithDash);
-      gitCommitLatestChangesAndSwitchToMain(res, blogTitleWithDash);
-      changeDirectory('..');
+      gitMergeBranchIntoMain(blog.title);
+      gitPushBranch('main');
 
       return res.status(200).json(`Blog: ${blog.title} saved successfully!`);
     } catch (err) {
--- src/routes/handlers/blog.ts
+++ src/routes/handlers/blog.ts
@@ -1,88 +1,102 @@
 import shell from 'shelljs';
 import { logger } from '../../logger/logger';
-import { Response } from 'express';
+import { Blog } from '../api/blog';
 
-export const pullLatestChangesToBlog = (res: Response) => {
-  if (shell.exec('git pull origin main').code !== 0) {
-    throw new Error('Error! Git update main failed!');
+export const saveBlog = (blog: Blog) => {
+  pullLatestChanges('main');
+  updateAndRebaseBlogWithRemote();
+  changeDirectory('Blogs');
+  switchToBlogTitleBranch(blog.title);
+  pullLatestChanges(blog.title);
+  writeBlogToFile(blog.content, blog.title);
+  replaceAll(/\\n/g, '\n\n', blog.title);
+  replaceAll(/\"/g, '', blog.title);
+  gitCommitLatestChangesAndSwitchToMain(blog.title);
+  changeDirectory('..');
+};
+
+export const gitMergeBranchIntoMain = (branchName: String) => {
+  if (
+    shell.exec(`git merge --strategy-option=theirs ${branchName}`).code !== 0
+  ) {
+    throw new Error('Git update main failed!');
   }
 };
 
-export const updateAndRebaseBlogWithRemote = (res: Response) => {
+export const pullLatestChanges = (branchName: string) => {
+  if (shell.exec(`git pull origin ${branchName}`).code !== 0) {
+    throw new Error('Git update main failed!');
+  }
+};
+
+export const updateAndRebaseBlogWithRemote = () => {
   if (shell.exec('git submodule update --remote --rebase').code !== 0) {
-    throw new Error('Error! Git update for submodule failed!');
+    throw new Error('Git update for submodule failed!');
   }
 };
 
 export const changeDirectory = (to: string) => {
   if (shell.cd(to).code !== 0) {
-    logger.error('Error! Already in Blogs!');
+    logger.error('Already in Blogs!');
   }
 };
 
-export const switchToBlogTitleBranch = (res: Response, fileName: String) => {
+export const switchToBlogTitleBranch = (fileName: String) => {
   if (shell.exec(`git show-ref -q --heads ${fileName}`).code !== 0) {
     if (shell.exec(`git checkout -b ${fileName}`).code !== 0) {
-      res.status(500).json('Error! Cannot checkout to new branch');
+      throw new Error('Cannot checkout to new branch');
     }
   } else {
     if (shell.exec(`git checkout ${fileName}`).code !== 0) {
-      res.status(500).json('Error! Cannot checkout to branch');
+      throw new Error('Cannot checkout to branch');
     }
   }
 };
 
-export const writeBlogToFile = (
-  res: Response,
-  content: String,
-  fileName: String
-) => {
+export const writeBlogToFile = (content: String, fileName: String) => {
   if (shell.exec(`echo "${content}" > ${fileName}.md`).code !== 0) {
-    throw new Error('Error! writing to file failed!');
+    throw new Error('writing to file failed!');
   }
 };
 
 export const replaceAll = (
-  res: Response,
   regex: RegExp,
   replacement: string,
   fileName: string
 ) => {
   if (shell.sed('-i', regex, replacement, `${fileName}.md`).code !== 0) {
-    throw new Error('Error! Sed failed!');
+    throw new Error('Sed failed!');
   }
 };
 
-export const gitCommitLatestChangesAndSwitchToMain = (
-  res: Response,
-  fileName: String
-) => {
+export const gitCommitLatestChangesAndSwitchToMain = (fileName: String) => {
   // add files and commit
   if (shell.exec('git add .').code !== 0) {
-    throw new Error('Error! Git add for submodule failed!');
+    throw new Error('Git add for submodule failed!');
   }
   if (shell.exec(`git commit -m "Updated ${fileName}.md"`).code !== 0) {
-    throw new Error('Error! Git commit for submodule failed!');
+    throw new Error('Git commit for submodule failed!');
   }
   if (shell.exec(`git push -u origin ${fileName}`).code !== 0) {
-    throw new Error('Error! Git update for submodule failed!');
+    throw new Error('Git update for submodule failed!');
   }
   if (shell.exec(`git checkout main`).code !== 0) {
-    throw new Error('Error! Git checkout to main failed!');
+    throw new Error('Git checkout to main failed!');
   }
 };
 
-export const gitCommitLatestChangesOnMainProject = (
-  res: Response,
-  fileName: String
-) => {
+export const gitCommitLatestChangesOnMainProject = (fileName: string) => {
   if (shell.exec('git add .').code !== 0) {
-    throw new Error('Error! Git add for main failed!');
+    throw new Error('Git add for main failed!');
   }
   if (shell.exec(`git commit -m "Updated ${fileName}.md"`).code !== 0) {
-    throw new Error('Error! Git commit for main failed!');
+    throw new Error('Git commit for main failed!');
   }
-  if (shell.exec(`git push`).code !== 0) {
-    throw new Error('Error! Git update for main failed!');
+  gitPushBranch(fileName);
+};
+
+export const gitPushBranch = (branchName: string) => {
+  if (shell.exec(`git push -u origin ${branchName}`).code !== 0) {
+    throw new Error(`Git push for ${branchName} failed!`);
   }
 };
