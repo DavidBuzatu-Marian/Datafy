5b657e5fb1f4cd8836af1c91354fef4bc9ee0629
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-08-14 15:13:44 +0100

    Refactored blogs

--- src/routes/api/blog.ts
+++ src/routes/api/blog.ts
@@ -2,8 +2,15 @@ import express from 'express';
 import { handleErrors, checkNotSucceeded } from '../handlers/errors';
 import { Request, Response } from 'express';
 import { check } from 'express-validator';
-import shell from 'shelljs';
-import { logger } from '../../logger/logger';
+import {
+  changeDirectory,
+  gitCommitLatestChangesAndSwitchToMain,
+  pullLatestChangesToBlog,
+  replaceAll,
+  switchToBlogTitleBranch,
+  updateAndRebaseBlogWithRemote,
+  writeBlogToFile,
+} from '../handlers/blog';
 
 const router = express.Router();
 
@@ -32,82 +39,16 @@ router.post(
       const blog: Blog = Object.assign(new Blog(), req.body);
       const blogTitleWithDash = blog.title.split(' ').join('-');
       // update local repo
-      if (shell.exec('git pull origin main').code !== 0) {
-        logger.error('Error! Git update main submodule failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      // run script to fetch remote changes
-      if (shell.exec('git submodule update --remote --rebase').code !== 0) {
-        logger.error('Error! Git update for submodule failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      // cd into submodule
-      shell.cd('Blogs');
-      // change to branch of blog title
-      if (
-        shell.exec(`git show-ref -q --heads ${blogTitleWithDash}`).code !== 0
-      ) {
-        // create branch
-        shell.exec(`git checkout -b ${blogTitleWithDash}`);
-      } else {
-        // checkout to existing brach
-        shell.exec(`git checkout ${blogTitleWithDash}`);
-      }
-      // // write to file
-      if (
-        shell.exec(`echo "${blog.content}" > ${blogTitleWithDash}.md`).code !==
-        0
-      ) {
-        logger.error('Error! writing to file failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      // replace \n
-      if (
-        shell.sed('-i', /\\n/g, '\n\n', `${blogTitleWithDash}.md`).code !== 0
-      ) {
-        logger.error('Error! Sed failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      // remove "
-      if (shell.sed('-i', /\"/g, '', `${blogTitleWithDash}.md`).code !== 0) {
-        logger.error('Error! Sed failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      // add files and commit
-      if (shell.exec('git add .').code !== 0) {
-        logger.error('Error! Git add for submodule failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      if (
-        shell.exec(`git commit -m "Updated ${blogTitleWithDash}.md"`).code !== 0
-      ) {
-        logger.error('Error! Git commit for submodule failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      if (shell.exec(`git push -u origin ${blogTitleWithDash}`).code !== 0) {
-        logger.error('Error! Git update for submodule failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      if (shell.exec(`git checkout main`).code !== 0) {
-        logger.error('Error! Git checkout to main failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      // update main
-      shell.cd('..');
-      if (shell.exec('git add .').code !== 0) {
-        logger.error('Error! Git add for main failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      if (
-        shell.exec(`git commit -m "Updated ${blogTitleWithDash}.md"`).code !== 0
-      ) {
-        logger.error('Error! Git commit for main failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
-      if (shell.exec(`git push`).code !== 0) {
-        logger.error('Error! Git update for main failed!');
-        return res.status(500).json('Error! Something went wrong');
-      }
+      pullLatestChangesToBlog(res);
+      updateAndRebaseBlogWithRemote(res);
+      changeDirectory('Blogs');
+      switchToBlogTitleBranch(res, blogTitleWithDash);
+      writeBlogToFile(res, blog.content, blogTitleWithDash);
+      replaceAll(res, /\\n/g, '\n\n', blogTitleWithDash);
+      replaceAll(res, /\"/g, '', blogTitleWithDash);
+      gitCommitLatestChangesAndSwitchToMain(res, blogTitleWithDash);
+      changeDirectory('..');
+
       return res.status(200);
     } catch (err) {
       handleErrors(res, err);
---
+++ src/routes/handlers/blog.ts
@@ -0,0 +1,101 @@
+import shell from 'shelljs';
+import { logger } from '../../logger/logger';
+import { Response } from 'express';
+
+export const pullLatestChangesToBlog = (res: Response) => {
+  if (shell.exec('git pull origin main').code !== 0) {
+    logger.error('Error! Git update main failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+};
+
+export const updateAndRebaseBlogWithRemote = (res: Response) => {
+  if (shell.exec('git submodule update --remote --rebase').code !== 0) {
+    logger.error('Error! Git update for submodule failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+};
+
+export const changeDirectory = (to: string) => {
+  if (shell.cd(to).code !== 0) {
+    logger.error('Error! Already in Blogs!');
+  }
+};
+
+export const switchToBlogTitleBranch = (res: Response, fileName: String) => {
+  if (shell.exec(`git show-ref -q --heads ${fileName}`).code !== 0) {
+    if (shell.exec(`git checkout -b ${fileName}`).code !== 0) {
+      logger.error('Error! Cannot checkout to new branch');
+      res.status(500).json('Error! Something went wrong');
+    }
+  } else {
+    if (shell.exec(`git checkout ${fileName}`).code !== 0) {
+      logger.error('Error! Cannot checkout to branch');
+      res.status(500).json('Error! Something went wrong');
+    }
+  }
+};
+
+export const writeBlogToFile = (
+  res: Response,
+  content: String,
+  fileName: String
+) => {
+  if (shell.exec(`echo "${content}" > ${fileName}.md`).code !== 0) {
+    logger.error('Error! writing to file failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+};
+
+export const replaceAll = (
+  res: Response,
+  regex: RegExp,
+  replacement: string,
+  fileName: string
+) => {
+  if (shell.sed('-i', regex, replacement, `${fileName}.md`).code !== 0) {
+    logger.error('Error! Sed failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+};
+
+export const gitCommitLatestChangesAndSwitchToMain = (
+  res: Response,
+  fileName: String
+) => {
+  // add files and commit
+  if (shell.exec('git add .').code !== 0) {
+    logger.error('Error! Git add for submodule failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+  if (shell.exec(`git commit -m "Updated ${fileName}.md"`).code !== 0) {
+    logger.error('Error! Git commit for submodule failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+  if (shell.exec(`git push -u origin ${fileName}`).code !== 0) {
+    logger.error('Error! Git update for submodule failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+  if (shell.exec(`git checkout main`).code !== 0) {
+    logger.error('Error! Git checkout to main failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+};
+
+export const gitCommitLatestChangesOnMainProject = (
+  res: Response,
+  fileName: String
+) => {
+  if (shell.exec('git add .').code !== 0) {
+    logger.error('Error! Git add for main failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+  if (shell.exec(`git commit -m "Updated ${fileName}.md"`).code !== 0) {
+    logger.error('Error! Git commit for main failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+  if (shell.exec(`git push`).code !== 0) {
+    logger.error('Error! Git update for main failed!');
+    return res.status(500).json('Error! Something went wrong');
+  }
+};
