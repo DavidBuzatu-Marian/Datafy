81a4e0180b2af33d52fa262a20cf64900f978214
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-08-15 16:02:36 +0100

    Added publish functional test

--- src/routes/api/__tests__/blog_functional_tests.ts
+++ src/routes/api/__tests__/blog_functional_tests.ts
@@ -16,3 +16,18 @@ describe('save blog content', () => {
     expect(response.statusCode).toEqual(200);
   }, 100000);
 });
+
+describe('publish blog content', () => {
+  it('should get blog title and content, save it, commit changes on blog submodule and merge branch with main', async () => {
+    const response = await request(app)
+      .put('/api/blog/publish')
+      .send({
+        title: 'This is a test blog',
+        content:
+          '<p>Posted on 10.10.2021</p>\\n<img src="https://i.ibb.co/C2K1gSk/pexels-lee-campbell-115655.jpg" alt="pexels-lee-campbell-115655"/>\\n# This is a test blog\\n\'["Database", "AWS"]\'\\n## This should be recognized as a sub-title\\n<div id="this should be recognized as a sub-title"><p>Mauris maximus nec ligula sed ullamcorper. Quisque semper eget felis ut semper. Sed sem leo, malesuada sit amet imperdiet eu, iaculis a nisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Mauris placerat eros id ligula posuere, at tincidunt orci pellentesque. Mauris lobortis tempus nisl, sit amet mollis nisl facilisis eu. Etiam congue libero ac est convallis, ac sollicitudin mi vulputate. Nunc sagittis mi malesuada augue bibendum pulvinar. Cras molestie ut leo eget vestibulum. In molestie sollicitudin metus eget facilisis. Cras sit amet cursus lorem.</p></div>\\n## This should be recognized as a sub-title as well\\n<div id="this should be recognized as a sub-title as well"><p>Phasellus eu dui ac urna maximus rutrum. Nullam sodales velit id nisl sagittis finibus. Donec porttitor pulvinar porttitor. Duis eget odio sed augue porta interdum placerat et mauris. Duis auctor purus dapibus, ultricies eros sed, pretium eros. Nunc a mi in odio sagittis luctus. Suspendisse nec dignissim libero. Sed tincidunt molestie blandit. Vestibulum accumsan lobortis elit, id rutrum risus rutrum a. Nunc elementum, nunc a suscipit ultrices, neque tortor euismod felis, ac ultricies diam augue ut odio. Integer eget varius elit. Etiam aliquam vehicula facilisis. Nulla eget sollicitudin tellus, et porta mi.\\n<p id="this should be recognized as a sub-title as well">Phasellus eu dui ac urna maximus rutrum. Nullam sodales velit id nisl sagittis finibus. Donec porttitor pulvinar porttitor. Duis eget odio sed augue porta interdum placerat et mauris. Duis auctor purus dapibus, ultricies eros sed, pretium eros. Nunc a mi in odio sagittis luctus. Suspendisse nec dignissim libero. Sed tincidunt molestie blandit. Vestibulum accumsan lobortis elit, id rutrum risus rutrum a. Nunc elementum, nunc a suscipit ultrices, neque tortor euismod felis, ac ultricies diam augue ut odio. Integer eget varius elit. Etiam aliquam vehicula facilisis. Nulla eget sollicitudin tellus, et porta mi.</p>\\n<p id="this should be recognized as a sub-title as well">Phasellus eu dui ac urna maximus rutrum. Nullam sodales velit id nisl sagittis finibus. Donec porttitor pulvinar porttitor. Duis eget odio sed augue porta interdum placerat et mauris. Duis auctor purus dapibus, ultricies eros sed, pretium eros. Nunc a mi in odio sagittis luctus. Suspendisse nec dignissim libero. Sed tincidunt molestie blandit. Vestibulum accumsan lobortis elit, id rutrum risus rutrum a. Nunc elementum, nunc a suscipit ultrices, neque tortor euismod felis, ac ultricies diam augue ut odio. Integer eget varius elit. Etiam aliquam vehicula facilisis. Nulla eget sollicitudin tellus, et porta sfsmi.' +
+          crypto.randomBytes(20).toString('hex') +
+          '</div>',
+      });
+    expect(response.statusCode).toEqual(200);
+  }, 100000);
+});
